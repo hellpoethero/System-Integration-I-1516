134e01e86ec76e34032b9673ad9bb89e2b2e6b1f
Đức Điệu <c iu>
2015-12-30 00:14:32 +0700

    tich hop chikitsa

--- resources/views/staff/add_category.blade.php
+++ resources/views/staff/add_category.blade.php
@@ -4,7 +4,7 @@
     <br>
     <center><h3>Thêm danh mục</h3></center>
     <br>
-    <form action="/health4all/staff/add/staff_category" method="post" accept-charset="utf-8" class="form-horizontal" role="form" id="add_staff_category">
+    <form action="https://localhost/health4all/staff/add/staff_category" method="post" accept-charset="utf-8" class="form-horizontal" role="form" id="add_staff_category">
         <div class="form-group">
             <div class="col-md-3">
                 <label for="first_name" class="control-label">Danh mục</label>
